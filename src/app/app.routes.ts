c56b4bf00dd5a8dcdac838d9a706f2deb91dfcc9
nicenicenice <112688217+nashie1004@users.noreply.github.com>
2025-05-31 13:36:47 +0800

    add simple routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { TodoContainerComponent } from './todo-container/todo-container.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        pathMatch: "full",
+        component: HomePageComponent,
+        title: "Home Page"
+    },
+    {
+        path: "login",
+        component: LoginPageComponent,
+        title: "Login Page"
+    },
+    {
+        path: "todo",
+        component: TodoContainerComponent,
+        title: "Todo Page"
+    }
+];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterOutlet],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
